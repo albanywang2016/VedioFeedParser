2dbf7d2149acaec13fbb6a7b8c97b006ff9fbf25
albanywang2016 <albanywang2016@gmail.com>
2017-04-19 16:58:13 -0400

    add new function PostToServer

--- src/com/davidwang/feed/test/TestFeedParser.java
+++ src/com/davidwang/feed/test/TestFeedParser.java
@@ -7,15 +7,21 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 
@@ -206,16 +212,48 @@ public class TestFeedParser {
 		}
 
 	}
+	
+	private void PostToServer(URL url, Map<String,Object> params) throws IOException{
+		
+        //URL url = new URL("http://example.net/new-message.php");
+
+//        params.put("name", "Freddie the Fish");
+//        params.put("email", "fishie@seamail.example.com");
+//        params.put("reply_to_thread", 10394);
+//        params.put("message", "Shark attacks in Botany Bay have gotten out of control. We need more defensive dolphins to protect the schools here, but Mayor Porpoise is too busy stuffing his snout with lobsters. He's so shellfish.");
+
+        StringBuilder postData = new StringBuilder();
+        for (Map.Entry<String,Object> param : params.entrySet()) {
+            if (postData.length() != 0) postData.append('&');
+            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+            postData.append('=');
+            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+        }
+        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+        conn.setDoOutput(true);
+        conn.getOutputStream().write(postDataBytes);
+
+        Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+        for (int c; (c = in.read()) >= 0;)
+            System.out.print((char)c);
+    
+	}
 
 	private static String writeToHTML(FeedItem item) throws FileNotFoundException, UnsupportedEncodingException {
 		// TODO Auto-generated method stub
 
 		String timestamp = item.getTimestamp();
-		String fileName = Const.XAMPP_FOLDER + item.getDayCreated() + Const.SLASH + timestamp + Const.DOT + Const.HTML;
+		//String fileName = Const.XAMPP_FOLDER + item.getDayCreated() + Const.SLASH + timestamp + Const.DOT + Const.HTML;
 		String fileNameHttp = Const.XAMPP_HTTP + item.getDayCreated() + Const.SLASH + timestamp + Const.DOT
 				+ Const.HTML;
 
-		PrintWriter writer = new PrintWriter(fileName, Const.UTF8);
+		PrintWriter writer = new PrintWriter(fileNameHttp, Const.UTF8);
 		writer.println("<html>");
 		writer.println("<head>");
 		writer.println("<meta charset=UTF-8>");
